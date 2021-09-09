94028831c61aaec671d1b597fff84f925243793e
Emma Strubell <emma.strubell@gmail.com>
2021-09-09 06:58:56 -0700

    Create capture_audio.py

---
+++ labs/lab0-setup/capture_audio.py
@@ -0,0 +1,10 @@
+
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+fs = 44100  # Sample rate
+seconds = 3  # Duration of recording
+
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+sd.wait()  # Wait until recording is finished
+write('output.wav', fs, myrecording)  # Save as WAV file 
