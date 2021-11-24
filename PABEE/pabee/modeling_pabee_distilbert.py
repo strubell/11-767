e853af7e32d37f2ebca57385c6312917f721e132
Jared Fernandez <jaredfern@cmu.edu>
2021-11-24 12:35:20 -0500

    add hw aware exit

--- PABEE/pabee/modeling_pabee_distilbert.py
+++ PABEE/pabee/modeling_pabee_distilbert.py
@@ -17,6 +17,7 @@
 
 
 import logging
+import time 
 
 import torch
 from torch import nn
@@ -178,6 +179,7 @@ class DistilBertModelWithPabee(DistilBertModel):
             pooled_output = activation(pre_classifier(encoder_outputs[0][:,0]))
             res = [output_layers[self.config.num_hidden_layers - 1](pooled_output)]
         else:
+            init_time = time.time()
             patient_counter = 0
             patient_result = None
             calculated_layer_num = 0
@@ -208,6 +210,8 @@ class DistilBertModelWithPabee(DistilBertModel):
 
                 patient_result = logits
                 if patient_counter == self.patience:
+                    break
+                if self.runtime_threshold > time.time() - init_time:
                     break
             res = [patient_result]
             self.inference_layers_num += calculated_layer_num
@@ -318,7 +322,7 @@ class DistilBertForSequenceClassificationWithPabee(DistilBertPreTrainedModel):
                 if total_loss is None:
                     total_loss = loss
                 else:
-                    total_loss += loss * (ix + 1)
+                    total_loss += loss * (len(logits) - ix)
                 total_weights += ix + 1
             outputs = (total_loss / total_weights,) + outputs
 
